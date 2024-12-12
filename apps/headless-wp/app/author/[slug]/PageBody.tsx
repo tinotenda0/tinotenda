19e10bab7b3202c44d7fab705bb280ef3acef019
Tinotenda Muzovaka <79769530+tinotenda0@users.noreply.github.com>
2024-12-12 18:00:19 +0000

    Fixed author page

--- apps/headless-wp/app/author/[slug]/PageBody.tsx
+++ apps/headless-wp/app/author/[slug]/PageBody.tsx
@@ -3,69 +3,72 @@ import Sidebar from '@/components/sidebar'
 import { getPosts } from '@/lib/wp-api'
 import { PostGridTwoCol } from '@metablog/ui'
 import React from 'react'
+import { Post } from '@/lib/wp-api/types' // Make sure this is correct
 
 const PageBody = ({
-   PostData,
-   categoryData,
+  PostData,
+  categoryData,
 }: {
-   PostData: any
-   categoryData: any
+  PostData: Post[] | null // Make sure the type matches the data shape
+  categoryData: any
 }) => {
-   const [showPerPage, setShowPerPage] = React.useState(8)
-   const [showMore, setShowMore] = React.useState(false)
-   const [allPosts, setAllPosts] = React.useState([])
+  const [showPerPage, setShowPerPage] = React.useState(8)
+  const [showMore, setShowMore] = React.useState(false)
+  
+  // Initialize as an empty array or null, but here empty array is convenient
+  const [allPosts, setAllPosts] = React.useState<Post[]>([])
 
-   React.useEffect(() => {
-      setAllPosts(PostData)
-   }, [PostData])
+  React.useEffect(() => {
+    // If PostData can be null, fallback to empty array
+    setAllPosts(PostData || [])
+  }, [PostData])
 
-   // *** get more posts ***
-   React.useEffect(() => {
-      if (showMore) {
-         const getMorePosts = async () => {
-            const { data } = await getPosts({ per_page: showPerPage })
-            setAllPosts(data)
-            setShowMore(false)
-         }
-         getMorePosts()
+  // *** get more posts ***
+  React.useEffect(() => {
+    if (showMore) {
+      const getMorePosts = async () => {
+        const { data } = await getPosts({ per_page: showPerPage })
+        // If data can be null, fallback to empty array
+        setAllPosts(data || [])
+        setShowMore(false)
       }
+      getMorePosts()
+    }
+  }, [showMore, showPerPage])
 
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-   }, [showMore, showPerPage])
-
-   return (
-      <section>
-         <div className="container mx-auto mt-12 mb-24 px-5 sm:px-0">
-            <div className="flex flex-col lg:grid lg:grid-cols-12 gap-8 items-start">
-               {/*  Body Component */}
-               <div className="col-span-12 lg:col-span-8">
-                  {/* all post collection Component */}
-                  <PostGridTwoCol data={PostData} />
-                  <div className="flex items-center justify-center mt-8">
-                     {/* if showMore true show loading */}
-                     {showMore && (
-                        <progress className="progress w-56 mx-auto"></progress>
-                     )}
-                     {/* show more button */}
-                     {showPerPage <= allPosts.length && (
-                        <button
-                           className="btn btn-outline btn-secondary font-work px-5"
-                           onClick={() => {
-                              setShowPerPage(showPerPage + 8)
-                              setShowMore(true)
-                           }}
-                        >
-                           Load More
-                        </button>
-                     )}
-                  </div>
-               </div>
-               {/*  Sidebar Component */}
-               <Sidebar data={PostData} categoryData={categoryData} />
+  return (
+    <section>
+      <div className="container mx-auto mt-12 mb-24 px-5 sm:px-0">
+        <div className="flex flex-col lg:grid lg:grid-cols-12 gap-8 items-start">
+          {/* Body Component */}
+          <div className="col-span-12 lg:col-span-8">
+            {/* all post collection Component */}
+            <PostGridTwoCol data={allPosts} />
+            <div className="flex items-center justify-center mt-8">
+              {/* if showMore true show loading */}
+              {showMore && (
+                <progress className="progress w-56 mx-auto"></progress>
+              )}
+              {/* show more button */}
+              {showPerPage <= allPosts.length && (
+                <button
+                  className="btn btn-outline btn-secondary font-work px-5"
+                  onClick={() => {
+                    setShowPerPage(showPerPage + 8)
+                    setShowMore(true)
+                  }}
+                >
+                  Load More
+                </button>
+              )}
             </div>
-         </div>
-      </section>
-   )
+          </div>
+          {/* Sidebar Component */}
+          <Sidebar data={allPosts} categoryData={categoryData} />
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default PageBody
